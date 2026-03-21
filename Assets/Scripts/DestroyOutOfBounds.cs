39172b645b3e8f7237cce4bbc91d7b5b489ba6c6
joanaccgomes <a63577@alunos.ipb.pt>
2026-03-21 17:54:49 +0000

    Game Over log

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -15,7 +15,9 @@ public class DestroyOutOfBounds : MonoBehaviour
     {
         if (transform.position.z > topBound) {
         Destroy(gameObject); 
+        
         } else if (transform.position.z < lowerBound){ 
+            Debug.Log("Game Over!");
             Destroy(gameObject);
           }
       
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,22 +7,25 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
 
+
+    private float startDelay =2;
+    private float spawnInterval =1.5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandemAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)){
-
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 
-            0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos,
-            animalPrefabs[animalIndex].transform.rotation);
-        }
+        
     }
+        void SpawnRandomAnimal(){
+           int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 
+             0, spawnPosZ);
+             Instantiate(animalPrefabs[animalIndex], spawnPos,
+              animalPrefabs[animalIndex].transform.rotation);}
 }
